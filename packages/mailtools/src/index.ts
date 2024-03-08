5cdb1bb3bbfa0ae1f7133d01cf158d232ac45d2d
BlankParticle <blankparticle@gmail.com>
2024-03-08 15:38:21 +0530

    chore: move mailtools to packages

---
+++ packages/mailtools/src/index.ts
@@ -0,0 +1,8 @@
+import linkify from './linkify';
+import { parseMessage } from './parseMessage';
+import { blockRemoteContent } from './blockRemoteContent';
+import type { ReplacementOptions } from './blockRemoteContent';
+import type { ParseMessageOptions } from './parseMessage';
+
+export { parseMessage, linkify, blockRemoteContent };
+export type { ParseMessageOptions, ReplacementOptions };
---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  endOfLine: 'lf',
+  semi: true,
+  printWidth: 80,
+  tabWidth: 2,
+  arrowParens: 'always',
+  overrides: [],
+  singleAttributePerLine: true
+};
