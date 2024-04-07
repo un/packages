b0af49f0c9aaf0b214a9d2ed7e4d2e2d41155557
BlankParticle <blankparticle@gmail.com>
2024-04-07 10:27:18 +0530

    chore: initial formatting fixes

--- apps/mailtools/src/components/ui/skeleton/skeleton.svelte
+++ apps/mailtools/src/components/ui/skeleton/skeleton.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-	import { cn } from "@lib/utils.js";
-	import type { HTMLAttributes } from "svelte/elements";
+  import { cn } from '@lib/utils.js';
+  import type { HTMLAttributes } from 'svelte/elements';
 
-	type $$Props = HTMLAttributes<HTMLDivElement>;
+  type $$Props = HTMLAttributes<HTMLDivElement>;
 
-	let className: $$Props["class"] = undefined;
-	export { className as class };
+  let className: $$Props['class'] = undefined;
+  export { className as class };
 </script>
 
-<div class={cn("animate-pulse rounded-md bg-muted", className)} {...$$restProps} />
+<div
+  class={cn('bg-muted animate-pulse rounded-md', className)}
+  {...$$restProps} />
